41e315903376b2ffcc6e6dbf6a9448970ef2c975
LucasDiezPe <lucas.diez.pena@gmail.com>
2024-07-26 16:30:05 -0300

    update

---
+++ Modulo_5/EjercicioModulo5/src/main/java/a/Persona.java
@@ -0,0 +1,25 @@
+package a;
+
+public class Persona {
+    private String Nombre;
+    private String DNI;
+
+
+    public String getDNI() {
+        return DNI;
+    }
+
+    public void setDNI(String DNI) {
+        this.DNI = DNI;
+    }
+
+
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+}
